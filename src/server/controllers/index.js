d07fd0bd26dd69ad0dd93cb5a35c2263d8a7fac1
François Hodierne <francois@hodierne.net>
2019-10-25 10:54:15 +0200

    feature: port api v1 payment-methods endpoint from API repository

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -2,6 +2,7 @@ import * as collectives from './collectives';
 
 import * as events from './events';
 import * as members from './members';
+import * as paymentMethods from './payment-methods';
 import * as transactions from './transactions';
 
 import accountOrders from './account-orders';
@@ -10,6 +11,7 @@ export default {
   collectives,
   events,
   members,
+  paymentMethods,
   transactions,
   accountOrders,
 };
