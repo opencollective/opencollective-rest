e1bfeeec9f6aab24ca441782725e070aeb0b1aab
Arman Bilge <armanbilge@gmail.com>
2021-08-25 00:41:12 -0700

    Enable cors at app-level

--- src/server/routes.js
+++ src/server/routes.js
@@ -9,6 +9,8 @@ const requireApiKey = (req, res, next) => {
 };
 
 export const loadRoutes = (app) => {
+  app.use(cors());
+
   app.get('/', (req, res) => {
     res.send('This is the Open Collective REST API.');
   });
@@ -21,24 +23,21 @@ export const loadRoutes = (app) => {
     res.send('User-agent: *\nDisallow: /');
   });
 
-  app.get('/:version(v1)?/:collectiveSlug.:format(json)', cors(), controllers.collectives.info);
-  app.get('/:version(v1)?/:collectiveSlug/members.:format(json|csv)', cors(), controllers.members.list);
+  app.get('/:version(v1)?/:collectiveSlug.:format(json)', controllers.collectives.info);
+  app.get('/:version(v1)?/:collectiveSlug/members.:format(json|csv)', controllers.members.list);
   app.get(
     '/:version(v1)?/:collectiveSlug/members/:backerType(all|users|organizations).:format(json|csv)',
-    cors(),
     controllers.members.list,
   );
   app.get(
     '/:version(v1)?/:collectiveSlug/tiers/:tierSlug/:backerType(all|users|organizations).:format(json|csv)',
-    cors(),
     controllers.members.list,
   );
 
-  app.get('/:version(v1)?/:collectiveSlug/events.:format(json)', cors(), controllers.events.list);
-  app.get('/:version(v1)?/:collectiveSlug/events/:eventSlug.:format(json)', cors(), controllers.events.info);
+  app.get('/:version(v1)?/:collectiveSlug/events.:format(json)', controllers.events.list);
+  app.get('/:version(v1)?/:collectiveSlug/events/:eventSlug.:format(json)', controllers.events.info);
   app.get(
     '/:version(v1)?/:collectiveSlug/events/:eventSlug/:role(attendees|followers|organizers|all).:format(json|csv)',
-    cors(),
     controllers.members.list,
   );
 
@@ -47,10 +46,9 @@ export const loadRoutes = (app) => {
   app.param('idOrUuid', idOrUuid);
 
   // Get transactions of a collective given its slug.
-  app.get('/v1/collectives/:collectiveSlug/transactions', cors(), requireApiKey, controllers.transactions.allTransactions);
+  app.get('/v1/collectives/:collectiveSlug/transactions', requireApiKey, controllers.transactions.allTransactions);
   app.get(
     '/v1/collectives/:collectiveSlug/transactions/:idOrUuid',
-    cors(),
     requireApiKey,
     controllers.transactions.getTransaction,
   );
@@ -59,13 +57,11 @@ export const loadRoutes = (app) => {
 
   app.get(
     '/v2/:slug/tier/:tierSlug/orders/:filter(incoming)?/:status(active|cancelled|error|paid|pending)?',
-    cors(),
     controllers.accountOrders,
   );
 
   app.get(
     '/v2/:slug/orders/:filter(incoming|outgoing)?/:status(active|cancelled|error|paid|pending)?',
-    cors(),
     controllers.accountOrders,
   );
 };
